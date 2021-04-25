bed280e1d4ca19db35a637bbc69e112bb3096acc
Vaishu <vaisumathy@gmail.com>
2021-04-25 13:13:52 +0530

    added tests for pester

---
+++ Tests/storageaccount.Tests.ps1
@@ -0,0 +1,14 @@
+﻿Param(
+    [Parameter(Mandatory = $true)]
+    [String]$rgName,
+    [Parameter(Mandatory = $true)]
+    [String]$storageName
+)
+Describe 'Compliance'{
+    Context 'Storage Account'{
+        It "storage account has no public access"{
+            $publicaccess = az storage account show -g $rgName -n $storageName --query "allowBlobPublicAccess"
+            $publicaccess | Should BeExactly "true"
+        }
+    } 
+}
